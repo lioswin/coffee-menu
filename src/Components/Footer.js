781d2f6438080f8c9746f6ae116ca713defc45cf
lioswin <lioswinrite@gmail.com>
2022-11-28 23:35:44 +0300

    final

---
+++ src/Components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Footer(){
+
+    return(
+        <>
+             <hr class="bottom-line"></hr>
+            <footer>
+            <p>
+          <a href="https://www.freecodecamp.org" target="_blank">Visit our website</a>
+        </p>
+        <p class="address">123 Free Code Camp Drive</p>
+            </footer>
+        </>
+    )
+
+}
+    
+
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <div>
+                 <h1>CAMPER CAFE</h1>
+        <p class="established">Est. 2020</p>
+        <hr></hr>
+        </div>
+    )
+}
+
+export default Header;
